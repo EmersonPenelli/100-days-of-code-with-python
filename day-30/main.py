5cef87f8554c1724969a997958312114385c071e
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-03-05 11:01:48 -0300

    Update main.py

--- day-30/main.py
+++ day-30/main.py
@@ -77,7 +77,7 @@ window.title("Password Manager")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="logo.png")
+logo_img = PhotoImage(file="d:\Projetos\Python Projects\Python-Projects\password_manager\logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
